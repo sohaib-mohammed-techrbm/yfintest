92c3384d80418ee062e55fe12e827ff125447ccc
sohaib8904 <s.md.sohaib8904@gmail.com>
2023-08-24 18:23:59 +0530

    return string1

--- prog_lang_app.py
+++ prog_lang_app.py
@@ -7,4 +7,6 @@ in_memory_datastore = {
 }
 @app.get('/programming_languages')
 def list_programming_languages():
-   return {"programming_languages":list(in_memory_datastore.values())}
+   #return {"programming_languages":list(in_memory_datastore.values())}
+   string1 = "testsend"
+   return string1
