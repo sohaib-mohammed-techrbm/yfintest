4acc3a10441e163fbb107f45cd0fe29f06fa852e
pankaj <pankaj.sangam@techrbm.com>
2023-08-28 18:00:35 +0530

    New Version of Yfin

---
+++ yfin_Data-V1.py
@@ -0,0 +1,8 @@
+tickers = ['DKS', 'M','ZM','LOW','SOS','TSLA','MRNA','MDT','NVDA','NVAX','FULC','NKE','BJ','MSFT','AXLA','ATVI','NVOS','S','COTY','PMN','FN','FL','ASO','CSIQ','GMVD','FRGT','APP','ASML','UBI.PA','AMC', 'AMZN',"GOOG",'AAPL'
+           'dks','m','zm','low','sos','tsla','mrna','mdt','nvda','nvax','fulc','nke','bj','msft','axla','atvi','nvos','s','coty','pmn','fn','fl','aso','csiq','gmvd','frgt','app','asml','ubi.pa','amc','amzn','goog','aapl']
+
+compare_list = ['compare','Compare','comparison','Comparison','cmp','Cmp','compair','Compair']
+
+high_revenue = ['High','high','Higest','higest','largest','Largest','topest','Topest','Top','top']
+
+low_revenue = ['Lowest','lowest','smallest','Smallest','down','Down']
